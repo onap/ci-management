52435d5ad7cfcfa335b77d2a0af774798c2bee4e
Andrew Grimberg <agrimberg@linuxfoundation.org>
2017-02-16 05:36:59 -0800

    Fix dcae raw file shiping
    Change-Id: Iaba7570a41168a68a56469436724aec0e2d7ceea
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- jjb/dcae/include-raw-pgaas.sh
+++ jjb/dcae/include-raw-pgaas.sh
@@ -6,25 +6,26 @@ echo "============== STARTING SCRIPT TO CREATE DEBIAN FILES ================="
 export BUILD_NUMBER="${BUILD_ID}"
 export PATH=$PATH:${WORKSPACE}/buildtools/bin
 
-#export OPENECOMP_NEXUS_RAW="https://nexus.openecomp.org/content/sites/raw"
-export OPENECOMP_NEXUS_RAW="${NEXUSPROXY}/content/sites/raw"
-export OPENECOMP_NEXUS_USER=$(xpath -q -e \
+export NEXUS_RAW="${NEXUSPROXY}/content/sites/raw"
+USER=$(xpath -q -e \
     "//servers/server[id='ecomp-raw']/username/text()" "$SETTINGS_FILE")
-export OPENECOMP_NEXUS_PASSWORD=$(xpath -q -e \
+PASS=$(xpath -q -e \
     "//servers/server[id='ecomp-raw']/password/text()" "$SETTINGS_FILE")
 
-echo $OPENECOMP_NEXUS_RAW
-echo $OPENECOMP_NEXUS_USER
-echo $OPENECOMP_NEXUS_PASSWORD
+# Create a netrc file for use with curl
+export NETRC=$(mktemp)
+echo "machine nexus.openecomp.org login ${USER} password ${PASS}" > "${NETRC}"
+
+echo $NEXUS_RAW
 
 export REPACKAGEDEBIANUPLOAD="set -x; curl -k \
-    --user '${OPENECOMP_NEXUS_USER}:${OPENECOMP_NEXUS_PASSWORD}' \
+    --netrc-file '${NETRC}' \
     --upload-file '{0}' \
-        '${OPENECOMP_NEXUS_RAW}/org.openecomp.dcae/deb-snapshots/{2}/{1}'"
+        '${NEXUS_RAW}/org.openecomp.dcae/deb-snapshots/{2}/{1}'"
 export REPACKAGEDEBIANUPLOAD2="set -x; curl -k \
-    --user '${OPENECOMP_NEXUS_USER}:${OPENECOMP_NEXUS_PASSWORD}' \
+    --netrc-file '${NETRC}' \
     --upload-file '{0}' \
-        '${OPENECOMP_NEXUS_RAW}/org.openecomp.dcae/deb-snapshots/{2}/{4}-LATEST.deb'"
+        '${NEXUS_RAW}/org.openecomp.dcae/deb-snapshots/{2}/{4}-LATEST.deb'"
 make debian
 echo "================= ENDING SCRIPT TO CREATE DEBIAN FILES ================="
 
