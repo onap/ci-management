88c1c894af87e9793206b3dd536491ce19d2679f
lj1412 <lji@research.att.com>
2017-02-17 22:21:06 +0000

    new script template and pgass' use
    Change-Id: I7e08f11433690e48a8751184abc955d3d5ec7446
    Signed-off-by: lj1412 <lji@research.att.com>

---
+++ jjb/dcae/script-pgaas-merge.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Create a debian package and push to remote repo
+#
+echo "============== STARTING SCRIPT TO CREATE DEBIAN FILES ================="
+
+export BUILD_NUMBER="${BUILD_ID}"
+export PATH=$PATH:${WORKSPACE}/buildtools/bin
+
+export NEXUS_RAW="${NEXUSPROXY}/content/sites/raw"
+USER=$(xpath -q -e \
+    "//servers/server[id='ecomp-raw']/username/text()" "$SETTINGS_FILE")
+PASS=$(xpath -q -e \
+    "//servers/server[id='ecomp-raw']/password/text()" "$SETTINGS_FILE")
+
+# Create a netrc file for use with curl
+export NETRC=$(mktemp)
+echo "machine nexus.openecomp.org login ${USER} password ${PASS}" > "${NETRC}"
+
+echo $NEXUS_RAW
+
+
+REPO="${NEXUS_RAW}/org.openecomp.dcae.pgaas/deb-snapshots"
+
+export REPACKAGEDEBIANUPLOAD="set -x; curl -k --netrc-file '${NETRC}' \
+    --upload-file '{0}' '${REPO}/{2}/{1}'"
+export REPACKAGEDEBIANUPLOAD2="set -x; curl -k --netrc-file '${NETRC}' \
+    --upload-file '{0}' '${REPO}/{2}/{4}-LATEST.deb'"
+make debian
+echo "================= ENDING SCRIPT TO CREATE DEBIAN FILES ================="
+
+#echo "============= STARTING SCRIPT TO CREATE JAVADOCS FILES ================"
+#make upload-javadocs
+#echo "============= ENDING SCRIPT TO CREATE JAVADOCS FILES =================="
