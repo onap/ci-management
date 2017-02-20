5c2e06ccec6125bc65988e63caa61747be058c3a
Pamela Dragosh <pdragosh@research.att.com>
2017-02-20 11:40:22 -0500

    prepend with openecomp and debug msg
    Change-Id: I09eeb886484c908e07eab50ee8272231eb475887
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- jjb/policy/include-raw-docker.sh
+++ jjb/policy/include-raw-docker.sh
@@ -6,11 +6,13 @@ echo '============== STARTING SCRIPT TO BUILD DOCKER IMAGES ================='
 DOCKER_REPOSITORY=nexus3.openecomp.org:10003
 
 for image in policy-os policy-nexus policy-db policy-base policy-drools policy-pe ; do
+    echo "Building $image"
     mkdir -p target/$image
     cp $image/* target/$image
-    docker build --quiet --tag ${DOCKER_REPOSITORY}/policy:$image target/$image
+    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy:$image target/$image
 done
 
 for image in policy-nexus policy-db policy-drools policy-pe; do
-    docker push ${DOCKER_REPOSITORY}/policy:$image
+    echo "Pushing $image"
+    docker push ${DOCKER_REPOSITORY}/openecomp/policy:$image
 done
