d2e934a909af9a1328fbc43b40d7c35b1573f69b
Pamela Dragosh <pdragosh@research.att.com>
2017-02-20 16:48:28 -0500

    fix tag name
    Change-Id: Id4a76ddd0fe4fc0d83d8014d44e4350f9f78da4e
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- jjb/policy/include-raw-docker.sh
+++ jjb/policy/include-raw-docker.sh
@@ -9,10 +9,10 @@ for image in policy-os policy-nexus policy-db policy-base policy-drools policy-p
     echo "Building $image"
     mkdir -p target/$image
     cp $image/* target/$image
-    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy:$image target/$image
+    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy/$image target/$image
 done
 
 for image in policy-nexus policy-db policy-drools policy-pe; do
     echo "Pushing $image"
-    docker push ${DOCKER_REPOSITORY}/openecomp/policy:$image
+    docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image
 done
