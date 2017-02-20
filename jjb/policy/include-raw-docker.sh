5cb53e7aa5a0789cedb6989e3d818c8db28a1db8
Pamela Dragosh <pdragosh@research.att.com>
2017-02-19 20:52:22 -0500

    need DOCKER_REPOSITORY set
    Change-Id: I6937a3e3ca7610ef16e7ee31ec387f2f0ca6fafb
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- jjb/policy/include-raw-docker.sh
+++ jjb/policy/include-raw-docker.sh
@@ -2,6 +2,11 @@
 #
 echo '============== STARTING SCRIPT TO BUILD DOCKER IMAGES ================='
 
+
+DOCKER_REPOSITORIES="nexus3.openecomp.org:10001 \
+                   nexus3.openecomp.org:10002 \
+                   nexus3.openecomp.org:10003"
+
 for image in policy-os policy-nexus policy-db policy-base policy-drools policy-pe ; do
     mkdir -p target/$image
     cp $image/* target/$image
