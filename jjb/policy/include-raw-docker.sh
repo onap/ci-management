ca4c0e0c8b9d0d8daf77d20fe16f23e3238b5b93
Pamela Dragosh <pdragosh@research.att.com>
2017-02-21 08:57:39 -0500

    moving to another local script
    Change-Id: I88282ef60e1e7d9756b2df739947f51653e0854b
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- jjb/policy/include-raw-docker.sh
+++ jjb/policy/include-raw-docker.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 #
-echo '============== STARTING SCRIPT TO BUILD DOCKER IMAGES ================='
+echo '============== CALLING SCRIPT TO BUILD DOCKER IMAGES ================='
 
+CURRENTDIR="$(pwd)"
+echo $CURRENTDIR
 
-DOCKER_REPOSITORY=nexus3.openecomp.org:10003
-
-for image in policy-os policy-nexus policy-db policy-base policy-drools policy-pe ; do
-    echo "Building $image"
-    mkdir -p target/$image
-    cp $image/* target/$image
-    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy/$image:1.0.0-SNAPSHOT target/$image
-    docker images
-done
-
-for image in policy-nexus policy-db policy-drools policy-pe; do
-    echo "Pushing $image"
-    docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image:1.0.0-SNAPSHOT
-done
+./docker_build.sh
