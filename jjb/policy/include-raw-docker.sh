fb9769f65dc4e73e950bd6e81a8438c5d65a92ca
Pamela Dragosh <pdragosh@research.att.com>
2017-02-20 22:42:05 -0500

    release snapshot for now
    Change-Id: If221023bb471f0d02bf6473632f0abd725b22847
    Signed-off-by: Pamela Dragosh <pdragosh@research.att.com>

--- jjb/policy/include-raw-docker.sh
+++ jjb/policy/include-raw-docker.sh
@@ -9,10 +9,11 @@ for image in policy-os policy-nexus policy-db policy-base policy-drools policy-p
     echo "Building $image"
     mkdir -p target/$image
     cp $image/* target/$image
-    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy/$image target/$image
+    docker build --quiet --tag ${DOCKER_REPOSITORY}/openecomp/policy/$image:1.0.0-SNAPSHOT target/$image
+    docker images
 done
 
 for image in policy-nexus policy-db policy-drools policy-pe; do
     echo "Pushing $image"
-    docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image
+    docker push ${DOCKER_REPOSITORY}/openecomp/policy/$image:1.0.0-SNAPSHOT
 done
