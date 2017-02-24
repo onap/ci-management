c27b091032fcccb928887467ed711b036cc034bf
Carsten Lund <lund@research.att.com>
2017-02-24 00:10:24 +0000

    Added some output to verify script
    Change-Id: I60fa5e6fa761d220045c8f89cb6ff6414731918f
    Signed-off-by: Carsten Lund <lund@research.att.com>

--- jjb/include-update-pom-versions.sh
+++ jjb/include-update-pom-versions.sh
@@ -12,15 +12,20 @@ source ./version.properties
 
 VERSION=$release_version
 
+echo Changing POM version to $VERSION
+
 ## handle POM files with no parent
 for file in $(find . -name pom.xml); do
     if [ "$(grep -c '<parent>' $file)" == "0" ]; then
+        echo Updating version in $file to $VERSION
         (
             cd $(dirname $file)
             ${MVN} versions:set versions:commit \
                 -DnewVersion=$VERSION \
                 -DprocessDependencies=false
         )
+        grep version $file
+        echo DONE $file
     fi
 done
 
