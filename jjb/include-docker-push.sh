da1af7c8a3be66a4de8254446bbfc18cff2e351c
Venkata Harish K Kajur <vk250x@att.com>
2017-02-24 14:28:27 +0000

    Change the docker push to be generic
    Change-Id: I2711dd352edcad1896809ef81259a08c9d9dbabb
    Signed-off-by: Venkata Harish K Kajur <vk250x@att.com>

--- jjb/include-docker-push.sh
+++ jjb/include-docker-push.sh
@@ -6,5 +6,7 @@ SEARCH="aai-service";
 if [[ $PROJECT =~ $SEARCH ]] ; then
     docker push $DOCKER_REPOSITORY/openecomp/ajsc-aai:latest;
 else
-    docker push $DOCKER_REPOSITORY/openecomp/model-loader:latest;
+    # Cut the prefix aai/ in example aai/model-loader
+    DOCKER_REPO_NAME=$(echo ${PROJECT} | cut -d"/" -f2-);
+    docker push $DOCKER_REPOSITORY/openecomp/${DOCKER_REPO_NAME}:latest;
 fi
