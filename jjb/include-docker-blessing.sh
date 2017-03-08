15fe7d606743f49e5f3b97b9eea082427bbd50f6
JulienBe <jb379x@att.com>
2017-03-08 13:07:21 -0800

    new job to release docker images
    pull docker image from release, tag it, push it to release
    
    Change-Id: I2418b5ae085fe09ed00f7ebaa6ceed4664c236ed
    Signed-off-by: JulienBe <jb379x@att.com>

---
+++ jjb/include-docker-blessing.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ -z "$VERSION" ];
+then
+    echo "Error: no version provided"
+    exit 1
+fi
+
+if [ -z "$TIMESTAMP" ];
+then
+    echo "Error: no timestamp provided"
+    exit 1
+fi
+
+RELEASE_REPOSITORY="nexus3.openecomp.org:10002"
+SNAPSHOT_REPOSITORY="nexus3.openecomp.org:10003"
+STAGING_IMAGE="openecomp/$PROJECT:$VERSION-STAGING-$TIMESTAMP"
+RELEASE_IMAGE="openecomp/$PROJECT:$VERSION"
+
+docker pull "$SNAPSHOT_REPOSITORY/$STAGING_IMAGE"
+docker tag "$SNAPSHOT_REPOSITORY/$STAGING_IMAGE" "$RELEASE_REPOSITORY/$RELEASE_IMAGE"
+docker push "$RELEASE_REPOSITORY/$RELEASE_IMAGE"
