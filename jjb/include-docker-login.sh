ccdcf7aa30bceb488bd73b59b15b4254f0883974
Andrew Grimberg <agrimberg@linuxfoundation.org>
2017-03-16 09:18:33 -0700

    Add new docker.staging repo on port 10004
    Change-Id: Icadc95d50e7192943e0771347635d4064e518a39
    Signed-off-by: Andrew Grimberg <agrimberg@linuxfoundation.org>

--- jjb/include-docker-login.sh
+++ jjb/include-docker-login.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# vim: ts=4 sw=4 sts=4 et :
+
 DOCKER_REPOSITORIES="nexus3.openecomp.org:10001 \
                    nexus3.openecomp.org:10002 \
-                   nexus3.openecomp.org:10003"
+                   nexus3.openecomp.org:10003 \
+                   nexus3.openecomp.org:10004"
 
 for DOCKER_REPOSITORY in $DOCKER_REPOSITORIES;
 do
