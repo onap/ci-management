338a5fcde402927a43319add21ac733b3923aff4
Jeremy Phelps <jphelps@linuxfoundation.org>
2017-03-28 15:38:24 -0500

    Change nexus3 url from ecomp to onap
    Change-Id: I8474346c8c37e2c25abc8a426e82cbad38bd656c
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

--- jjb/include-docker-login.sh
+++ jjb/include-docker-login.sh
@@ -2,10 +2,10 @@
 
 # vim: ts=4 sw=4 sts=4 et :
 
-DOCKER_REPOSITORIES="nexus3.openecomp.org:10001 \
-                   nexus3.openecomp.org:10002 \
-                   nexus3.openecomp.org:10003 \
-                   nexus3.openecomp.org:10004"
+DOCKER_REPOSITORIES="nexus3.onap.org:10001 \
+                   nexus3.onap.org:10002 \
+                   nexus3.onap.org:10003 \
+                   nexus3.onap.org:10004"
 
 for DOCKER_REPOSITORY in $DOCKER_REPOSITORIES;
 do
