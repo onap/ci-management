a82ccdf169c844f4a05efa48f1a17c40def859db
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-08-31 11:52:55 -0700

    Add gecko driver for robotframework
    Add gecko driver for docker based builds that will
    run Selerium UI tests
    
    Issue-id: CLAMP-28
    Change-Id: Ib0fdb21165a5814dc7cb6599fc5226d35db813ee
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- packer/provision/docker.sh
+++ packer/provision/docker.sh
@@ -34,6 +34,9 @@ ubuntu_systems() {
 
     # Install dependencies for robotframework and robotframework-sshlibrary
     apt install -y unzip sshuttle netcat libffi-dev libssl-dev
+    wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+    tar xvzf geckodriver-v0.18.0-linux64.tar.gz -C /tmp
+    export PATH=$PATH:/tmp
 
     # Install docker
     apt install -y docker.io
