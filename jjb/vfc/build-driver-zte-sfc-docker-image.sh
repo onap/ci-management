1ca7549470af0b5105a3b95b1770942cce548ed4
fujinhua <fu.jinhua@zte.com.cn>
2017-09-08 08:47:25 +0800

    Update docker build script of vfc-driver-sfc
    Fix path of build_image.sh to the path in source directory
    
    Change-Id: I95042945f2bd7b33e15f3bf5fd7d93f9628cedd4
    Issue-Id: CIMAN-61
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- jjb/vfc/build-driver-zte-sfc-docker-image.sh
+++ jjb/vfc/build-driver-zte-sfc-docker-image.sh
@@ -5,7 +5,7 @@ echo '============== CALLING SCRIPT TO BUILD DOCKER IMAGES ================='
 CURRENTDIR="$(pwd)"
 echo $CURRENTDIR
 
-chmod 755 ./docker/*.*
+chmod 755 ./zte/sfc-driver/plugin-standalone/src/main/assembly/docker/*.*
 
-./docker/build_image.sh
+./zte/sfc-driver/plugin-standalone/src/main/assembly/docker/build_image.sh
 
