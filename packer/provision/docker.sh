62a7edb751f862ac6a921359802b1c39e5873908
Gary Wu <gary.i.wu@huawei.com>
2017-09-19 11:46:08 -0700

    Add write permission to /data
    Create a /data directory on the VM that some ONAP
    containers need to mount to retrieve environment
    configuration data.
    
    Change-Id: I4ec91822f841c4b269d768f660f47ffecd243267
    Issue-ID: INT-216
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- packer/provision/docker.sh
+++ packer/provision/docker.sh
@@ -47,6 +47,11 @@ all_systems() {
     curl -o /usr/local/bin/docker-compose -L "https://github.com/docker/compose/releases/download/1.15.0/docker-compose-$(uname -s)-$(uname -m)"
     chmod +x /usr/local/bin/docker-compose
     docker-compose -v
+
+
+    # INT-216 add write permission to /data
+    mkdir -p /data
+    chmod a+rw /data
 }
 
 echo "---> Detecting OS"
