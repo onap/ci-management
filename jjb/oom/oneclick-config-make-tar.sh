ceea1648f5bf1daeb195f1931bf756b6d1d900a8
gfraboni <gino.fraboni@amdocs.com>
2017-09-27 11:24:35 -0400

    JJB templates to build One-Click Config docker img
    Templates for a Jenkins Job to build and publish the Docker image
    for the One-Click configuration pod.
    
    Issue-ID: OOM-327
    
    Change-Id: I45322a220c573d5037104d1a68b58a4541ac4964
    Signed-off-by: gfraboni <gino.fraboni@amdocs.com>

---
+++ jjb/oom/oneclick-config-make-tar.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+echo '========= CALLING SCRIPT TO BUILD ONECLICK CONFIG TAR FILE ========='
+
+CURRENTDIR="$(pwd)"
+echo $CURRENTDIR
+
+cd kubernetes/config/docker/init
+
+chmod 755 *.*
+
+./make-tar.sh
