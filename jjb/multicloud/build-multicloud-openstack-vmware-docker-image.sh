c06e1c8e810527d18b275cdae6a42d251febb3de
Arun Arora <aroraarun@vmware.com>
2017-10-10 17:04:40 +0530

    Jenkins Jobs for vmware vesagent
    Add new yaml for vesagent Java Verify, Merge, Release and Docker jobs
    Remove vesagent jobs from vmware vio yaml
    Incorporate Review Comment to fix mvn-settings value
    
    Change-Id: Ibbbb7a15cbf85381121e555c3f1e86d0ab5ec5ab
    Issue-ID: MULTICLOUD-8
    Signed-off-by: Arun Arora <aroraarun@vmware.com>

--- jjb/multicloud/build-multicloud-openstack-vmware-docker-image.sh
+++ jjb/multicloud/build-multicloud-openstack-vmware-docker-image.sh
@@ -9,6 +9,3 @@ chmod 755 ./vio/docker/*.*
 
 ./vio/docker/build_image.sh
 
-echo '========================== Building docker for vesagent =========================='
-chmod 755 ./vesagent/docker/*.*
-./vesagent/docker/docker-build.sh
