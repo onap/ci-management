0eaada84aa2c5b58757c706503c83cebfb6c657f
Jessica Wagantall <jwagantall@linuxfoundation.org>
2017-10-19 08:51:03 -0700

    Install python3.6-dev package in packer
    Add python3.6-dev package which is needed by
    pyCrypto.
    
    Change-Id: Iaaa3fb8ca8d464e02af71ae50656861a5d104076
    Issue-ID: CIMAN-108
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

--- packer/provision/docker.sh
+++ packer/provision/docker.sh
@@ -35,7 +35,7 @@ ubuntu_systems() {
     # Install Python3.6
     sudo add-apt-repository -y ppa:jonathonf/python-3.6
     sudo apt-get update
-    sudo apt-get install -y python3.6
+    sudo apt-get install -y python3.6 python3.6-dev
 
     # Install dependencies for robotframework and robotframework-sshlibrary
     apt install -y unzip sshuttle netcat libffi-dev libssl-dev
