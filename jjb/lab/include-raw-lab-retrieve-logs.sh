ee95ea5f6e901d52f95d15fcbf5f3a204b52b82d
Gary Wu <gary.i.wu@huawei.com>
2018-01-25 16:06:29 -0800

    Change lab jobs to pull logs from lab CI systems
    It turns out that the "External Job" job type cannot
    integrate with the Jenkins Robot plugin.
    
    So, instead of that, this defines the lab jobs to retrieve
    the logs (including Robot logs) from the lab CI systems.
    These logs can then be post-processed by the Jenkins Robot
    plugin as well as get archived to LF Nexus by LF Jenkins,
    without the lab CI systems needing to upload logs directly.
    
    These lab jobs will be remotely triggered via REST API calls by
    the lab CI systems as the lab CI jobs complete.
    
    Change-Id: I3b793088c459ebb1219aee80fd43cf669a62128a
    Issue-ID: INT-278
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

---
+++ jjb/lab/include-raw-lab-retrieve-logs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -xe -o pipefail
+
+echo Job triggered by $SRC_BUILD_URL
+echo Retriving logs from $LOG_DIR_URL
+
+rm -rf archives
+wget -r -nv -nd --no-parent -l 1 --reject="index.html*" -P archives "$LOG_DIR_URL"
+cat archives/console.log
