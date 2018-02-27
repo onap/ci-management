0470b360fff8c3acda455cc9b4cbc95256d1bcae
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2018-02-27 15:10:17 -0800

    Configure PyPI releases
    This changes adds the following functionality for PyPI releases
    
    - macros
     - A PyPI server macro
    
    - scripts
     - A script to build artifacts
     - A script to publish artifacts
    
    - templates
     - Templates for release
    
    - jobs
     - Jobs for release
       - Nexus3 staging
       - Nexus3 release
       - PyPI index will be added at a later date
    
    Issue-ID: CIMAN-137
    Change-Id: I53522ed2fa2f462afebaac94c44fa2a9979e9c2a
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ shell/pypi-dist-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to build PyPI artifacts
+set -e -x -o pipefail
+
+virtualenv /tmp/v/twine
+source "/tmp/v/twine/bin/activate"
+
+pip install twine wheel
+
+cd "$WORKSPACE/$TOX_DIR"
+python setup.py sdist bdist_wheel
