7087ff627c74cea2ccf09ee0d9dc2b3447688069
Gary Wu <gary.i.wu@huawei.com>
2018-05-03 16:03:24 -0700

    Change source console timestamp log file name
    Change source console timestamp log file name to
    avoid having it overwritten by the local Jenkins.
    
    Change-Id: I16e4ab20064e013366884de9964f5eeb0b8ce82f
    Issue-ID: INT-401
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- jjb/lab/include-raw-lab-retrieve-logs.sh
+++ jjb/lab/include-raw-lab-retrieve-logs.sh
@@ -5,10 +5,11 @@ echo Job triggered by $SRC_BUILD_URL
 echo Retriving logs from $LOG_DIR_URL
 
 rm -rf archives
+mkdir -p archives
+curl -f "$SRC_BUILD_URL/timestamps/?time=HH:mm:ssZ&appendLog" > archives/console-source-timestamp.log
 wget -r -nv -nd --no-parent -l 1 --reject="index.html*" -P archives "$LOG_DIR_URL"
-curl -f "$SRC_BUILD_URL/timestamps?time=HH:mm:ssZ&appendLog" > archives/console-timestamp.log
-if [ -s archives/console-timestamp.log ]; then
-    cat archives/console-timestamp.log
+if [ -s archives/console-source-timestamp.log ]; then
+    cat archives/console-source-timestamp.log
 else
     cat archives/console.log
 fi
