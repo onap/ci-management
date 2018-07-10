5583a11b6b0344b780454606bd2e03188d95087c
Huang Haibin <haibin.huang@intel.com>
2018-07-10 21:17:51 +0800

    Add pike subproject
    Change-Id: I37661626fb7c2a7e4051197af92c474ee549256f
    Issue-ID: MULTICLOUD-276
    Signed-off-by: Huang Haibin <haibin.huang@intel.com>

--- jjb/multicloud/build-multicloud-openstack-docker-image.sh
+++ jjb/multicloud/build-multicloud-openstack-docker-image.sh
@@ -8,7 +8,9 @@ echo $CURRENTDIR
 chmod 755 ./newton/docker/*.*
 chmod 755 ./ocata/docker/*.*
 chmod 755 ./windriver/docker/*.*
+chmod 755 ./pike/docker/*.*
 
 ./newton/docker/build_image.sh
 ./ocata/docker/build_image.sh
 ./windriver/docker/build_image.sh
+./pike/docker/build_image.sh
