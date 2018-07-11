f4257a7495b191c07d935c61c3aeb3f6a87fb0f1
Jessica Wagantall <jwagantall@linuxfoundation.org>
2018-07-11 13:48:56 -0700

    Manage global env vars
    Manage Jenkins global environmental global
    variables via ci-management.
    
    Change-Id: I3ac87e0944b7994b2b1049742c3def34e37a5508
    Issue-ID: CIMAN-176
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>

---
+++ jenkins-config/global-vars-production.sh
@@ -0,0 +1,15 @@
+DOCKER_REGISTRY=nexus3.onap.org
+DOCKERHUB_REGISTRY=docker.io
+GERRIT_URL=https://gerrit.onap.org/r
+GIT_BASE=ssh://ecomp-jobbuilder@gerrit.onap.org:29418/$PROJECT
+GIT_NO_PROJECT=ssh://ecomp-jobbuilder@gerrit.onap.org:29418
+GIT_URL=ssh://ecomp-jobbuilder@gerrit.onap.org:29418
+JENKINS_HOSTNAME=vex-yul-ecomp-jenkins-1
+JIRA_URL=https://jira.onap.org
+LOGS_REPO_URL=https://nexus.onap.org/service/local/repositories/logs
+LOGS_SERVER=https://logs.onap.org
+NEXUS_URL=https://nexus.onap.org
+NEXUSPROXY=https://nexus.onap.org
+REGISTRY_PORTS=10001 10002 10003 10004
+SILO=production
+SONAR_URL=https://sonar.onap.org
