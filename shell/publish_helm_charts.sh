b4bc7395b039a81759a9351243da0d11923beac8
Jeremy Phelps <jphelps@linuxfoundation.org>
2018-08-15 16:26:12 -0500

    Add helm-chart publishing script
    Issue-ID: CIMAN-164
    Change-Id: Iae6f765c2f8a7953a308f2ce81e79e4f633a1a2e
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ shell/publish_helm_charts.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e -o pipefail
+cd kubernetes/dist/packages/ || exit
+while IFS= read -a line; do
+    charts+=( "$line" )
+done < <( ls )
+
+for chart in "${charts[@]}";
+do
+  chart=$(echo "$chart" | xargs)
+  case $BUILD_TYPE in
+    'snapshot')
+      curl -n --upload-file $chart https://nexus.onap.org/content/sites/oom-helm-$BUILD_TYPE/$chart
+      curl -n --upload-file $chart https://nexus.onap.org/content/sites/oom-helm-$BUILD_TYPE/$GIT_COMMIT/$chart
+      ;;
+    'staging')
+      curl -n --upload-file $chart https://nexus.onap.org/content/sites/oom-helm-$BUILD_TYPE/$chart
+      curl -n --upload-file $chart https://nexus.onap.org/content/sites/oom-helm-$BUILD_TYPE/$GIT_COMMIT/$chart
+      ;;
+    'release')
+      echo "Release automation not implemented yet."
+      exit 1
+        ;;
+    *)
+      echo "You must set BUILD_TYPE to one of (snapshot, staging, release)."
+      exit 1
+      ;;
+  esac
+done
+cd ../../../
