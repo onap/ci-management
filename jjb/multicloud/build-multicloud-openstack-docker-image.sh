f7d427fff43d41f2cb095ff760eca80e0276951a
Haibin Huang <haibin.huang@intel.com>
2019-01-15 13:11:42 +0800

    Support openstack starlinx plugin
    Change-Id: Ic158ab2c2b0900b882834c0ac07d2c16cdd58152
    Issue-ID: MULTICLOUD-428
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>

--- jjb/multicloud/build-multicloud-openstack-docker-image.sh
+++ jjb/multicloud/build-multicloud-openstack-docker-image.sh
@@ -9,8 +9,10 @@ chmod 755 ./newton/docker/*.*
 chmod 755 ./ocata/docker/*.*
 chmod 755 ./windriver/docker/*.*
 chmod 755 ./pike/docker/*.*
+chmod 755 ./starlingx/docker/*.*
 
 ./newton/docker/build_image.sh
 ./ocata/docker/build_image.sh
 ./windriver/docker/build_image.sh
 ./pike/docker/build_image.sh
+./starlingx/docker/build_image.sh
