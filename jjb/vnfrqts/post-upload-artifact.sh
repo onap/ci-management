fc540d4f456535bd113429092ecd30b43ee62f23
Hagop Bozawglanian <hagop.bozawglanian@att.com>
2019-02-26 21:42:22 +0000

    VNFRQTS - Fix curl statement
    Update the curl statement and changes to support the statement.
    
    Change-Id: I5a06b93b1e4a3b9ac9c64055eac2a45d71d71384
    Issue-ID: VNFRQTS-538
    Signed-off-by: Hagop Bozawglanian <hagop.bozawglanian@att.com>

--- jjb/vnfrqts/post-upload-artifact.sh
+++ jjb/vnfrqts/post-upload-artifact.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 set -e -o pipefail
-echo "-n --upload-file $ARTIFACT_NAME https://nexus.onap.org/content/sites/raw/$JOB_BASE_NAME/$GERRIT_BRANCH/$ARTIFACT_NAME"
-curl -n --upload-file "$ARTIFACT_NAME" "https://nexus.onap.org/content/sites/raw/$JOB_BASE_NAME/$GERRIT_BRANCH/$ARTIFACT_NAME"
+cd $WORKSPACE/docs/data
+echo "-n --upload-file $ARTIFACT_NAME https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$ARTIFACT_NAME"
+curl -n --upload-file "$ARTIFACT_NAME" "https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$ARTIFACT_NAME"
