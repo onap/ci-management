107ee27481a9583c42c3b9c0a21030fb660da91a
Hagop Bozawglanian <hagop.bozawglanian@att.com>
2019-02-28 19:09:30 +0000

    VNFRQTS - Update job to upload artifacts
    Update the post script to curl raw artifacts
    based on the number passed in to work for
    VNFRQTS and VVP.
    
    Change-Id: Iefd4e0e5069c2917952072dbf5e26433495fd25f
    Issue-ID: VNFRQTS-539
    Signed-off-by: Hagop Bozawglanian <hagop.bozawglanian@att.com>

--- jjb/vnfrqts/post-upload-artifact.sh
+++ jjb/vnfrqts/post-upload-artifact.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 set -e -o pipefail
-cd $WORKSPACE/docs/data
-echo "-n --upload-file $ARTIFACT_NAME https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$ARTIFACT_NAME"
-curl -n --upload-file "$ARTIFACT_NAME" "https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$ARTIFACT_NAME"
+set -- $ARTIFACT_NAME
+if [ -z "$ARTIFACT_NAME" ]
+then
+  echo "ERROR NO ARTIFACTS ENTERED"
+else
+	if [ -z "$2" ]
+	then
+		cd $WORKSPACE/docs/data
+		echo "-n --upload-file $ARTIFACT_NAME https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$ARTIFACT_NAME"
+		curl -n --upload-file "$ARTIFACT_NAME" "https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$ARTIFACT_NAME"
+	else
+		cd $WORKSPACE/ice_validator/output
+		echo "-n --upload-file $1 https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$1"
+		curl -n --upload-file "$1" "https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$1"
+		echo "-n --upload-file $2 https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$2"
+		curl -n --upload-file "$2" "https://nexus.onap.org/content/sites/raw/$PROJECT_ID/$GERRIT_BRANCH/$2"
+	fi
+fi
---
+++ jjb/vvp/pre-build-csv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+virtualenv -p python3 $WORKSPACE/venv-tox
+source $WORKSPACE/venv-tox/bin/activate
+pip3 install --upgrade pip
+pip3 install --no-use-pep517 -r requirements.txt
+ls
+wget -O $WORKSPACE/ice_validator/heat_requirements.json "https://nexus.onap.org/content/sites/raw/org.onap.vnfrqts.requirements/master/needs.json"
+echo "Copied newest needs."
+cd ice_validator/
+pytest --self-test tests/
