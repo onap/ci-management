43476fad1cdbd5eff563ff2d60a0e22e80a5815a
Bin Yang <bin.yang@windriver.com>
2019-03-19 08:02:52 +0000

    Add jjb for multicloud artifactbroker
    Change-Id: Ie84cda291efe50c1e018c81d3c912757aeeac155
    Issue-ID: MULTICLOUD-544
    Signed-off-by: Bin Yang <bin.yang@windriver.com>

--- jjb/multicloud/build-multicloud-framework-docker-image.sh
+++ jjb/multicloud/build-multicloud-framework-docker-image.sh
@@ -6,5 +6,7 @@ CURRENTDIR="$(pwd)"
 echo $CURRENTDIR
 
 chmod 755 ./multivimbroker/docker/*.*
+chmod 755 ./artifactbroker/docker/*.*
 
 ./multivimbroker/docker/build_image.sh
+./artifactbroker/docker/build_image.sh
