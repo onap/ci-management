e866ab89bf4b7c4fc60808b81a466fb501ce31ad
Hagop Bozawglanian <hagop.bozawglanian@att.com>
2019-05-03 22:37:55 +0000

    VNFRQTS - Removing wget to store artifact locally
    Because of connectivity, the JSON isn't being pulled down and being
    overwritten. The file will be stored locally in VNFRQTS and uploaded
    to nexus instead of keeping source of truth in nexus.
    
    Change-Id: Ia333f385eb84178103b10beb31ea3a71bc72c134
    Issue-ID: VNFRQTS-636
    Signed-off-by: Hagop Bozawglanian <hagop.bozawglanian@att.com>

--- jjb/vnfrqts/pre-build-needs.sh
+++ jjb/vnfrqts/pre-build-needs.sh
@@ -5,5 +5,4 @@ source $WORKSPACE/venv-tox/bin/activate
 pip install --upgrade pip
 pip install -r etc/requirements.txt
 ls
-wget -O $WORKSPACE/docs/data/needs.json "https://nexus.onap.org/content/sites/raw/org.onap.vnfrqts.requirements/master/needs.json"
 tox -e needs
