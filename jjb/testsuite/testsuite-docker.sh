22d037a5fd0fc932fb6d3b820f7a4ae1d5a4d1e8
Gary Wu <gary.i.wu@huawei.com>
2019-05-08 21:24:46 -0700

    Add timestamp tag for testsuite docker image
    Change-Id: I0cae9a59c314111acff1bad9af32d65a03de36fe
    Issue-ID: INT-1070
    Signed-off-by: Gary Wu <gary.i.wu@huawei.com>

--- jjb/testsuite/testsuite-docker.sh
+++ jjb/testsuite/testsuite-docker.sh
@@ -4,7 +4,11 @@ echo '============== CALLING SCRIPT TO CREATE DOCKER IMAGES ================='
 cp $WORKSPACE/docker/* .
 docker -D build -t onap/testsuite .
 export REPO="nexus3.onap.org:10003"
-for tag in $tags
+
+DATETIME_STAMP=$(date +%Y%m%dT%H%M%S)
+STAGING_TAG=${base_version}-STAGING-${DATETIME_STAMP}Z
+
+for tag in $tags $STAGING_TAG
 do
   docker tag onap/testsuite:latest $REPO/onap/testsuite:$tag
   docker push $REPO/onap/testsuite:$tag
