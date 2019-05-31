5e8b5a136dbb808ddbf99a22d0710862371225ae
tianxing <15210838572@139.com>
2019-05-31 18:00:01 +0800

    fix build-nfvo-svnfm-driver-docker-image
    Change-Id: I2624df1d03e77b6d6f110095ef82a51e707ad31c
    Issue-ID: VFC-1404
    Signed-off-by: tianxing <15210838572@139.com>

--- jjb/vfc/build-nfvo-svnfm-driver-docker-image.sh
+++ jjb/vfc/build-nfvo-svnfm-driver-docker-image.sh
@@ -13,7 +13,4 @@ echo '============== BUILDING HUAWEI IMAGE ================='
 chmod 755 ./huawei/vnfmadapter/VnfmadapterService/docker/*.*
 ./huawei/vnfmadapter/VnfmadapterService/docker/build_image.sh
 
-echo '============== BUILDING NOKIA V1 IMAGE ================='
-chmod 755 ./nokia/vnfmdriver/vfcadaptorservice/docker/*.*
-./nokia/vnfmdriver/vfcadaptorservice/docker/build_image.sh
 
