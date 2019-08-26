c663d97384ef543805ad108a280803541c53ef8c
Haibin Huang <haibin.huang@intel.com>
2019-08-26 14:00:34 +0800

    Delete ci for ocata
    Issue-ID: MULTICLOUD-775
    Signed-off-by: Haibin Huang <haibin.huang@intel.com>
    Change-Id: I905263c391b12db009db23ecde63bba093b7aeb8

--- jjb/multicloud/build-multicloud-openstack-docker-image.sh
+++ jjb/multicloud/build-multicloud-openstack-docker-image.sh
@@ -5,14 +5,12 @@ echo '============== CALLING SCRIPT TO BUILD DOCKER IMAGES ================='
 CURRENTDIR="$(pwd)"
 echo $CURRENTDIR
 
-chmod 755 ./ocata/docker/*.*
 chmod 755 ./windriver/docker/*.*
 chmod 755 ./pike/docker/*.*
 chmod 755 ./starlingx/docker/*.*
 #chmod 755 ./lenovo/docker/*.*
 chmod 755 ./fcaps/docker/*.*
 
-./ocata/docker/build_image.sh
 ./windriver/docker/build_image.sh
 ./pike/docker/build_image.sh
 ./starlingx/docker/build_image.sh
