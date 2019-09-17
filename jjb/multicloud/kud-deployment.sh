da5f2671d8496369498629f618d617406f924445
Konrad Bańka <k.banka@samsung.com>
2019-09-17 10:23:37 +0200

    DEBUG: Troubleshooting virtualization issues on jenkins slave
    Issue-ID: MULTICLOUD-686
    
    Signed-off-by: Konrad Bańka <k.banka@samsung.com>
    Change-Id: I85df5ca36fb017c1815053e2204568b8708d0620

--- jjb/multicloud/kud-deployment.sh
+++ jjb/multicloud/kud-deployment.sh
@@ -2,4 +2,12 @@
 
 set -x -e -o pipefail
 cd ./kud/ci
+
+export KUD_PLUGIN_ENABLED=false
+export KUD_ENABLE_TESTS=true
+export KUD_ADDONS='virtlet'
+
+#Increase logs verbosity
+git fetch "https://gerrit.onap.org/r/multicloud/k8s" refs/changes/22/93022/5 && git cherry-pick FETCH_HEAD
+
 bash -x ./kud-installer.sh
