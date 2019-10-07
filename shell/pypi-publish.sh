6555db64fdc8e7b8cd76bbd0ec76564b9f3ad6d7
DW Talton <dtalton@contractor.linuxfoundation.org>
2019-10-07 13:56:49 -0700

    Correct python environment variable
    The python environment variable for virtualenv should be
    $PYTHON not $PYTHON_VERSION.
    
    Issue-ID: CIMAN-1
    Signed-off-by: DW Talton <dtalton@contractor.linuxfoundation.org>
    Change-Id: I975bc16efd572ae56b003fde134bed82178de6ff

--- shell/pypi-publish.sh
+++ shell/pypi-publish.sh
@@ -2,7 +2,7 @@
 # Script to publush PyPI artifacts
 set -e -x -o pipefail
 
-virtualenv -p "$PYTHON_VERSION" /tmp/v/twine
+virtualenv -p "$PYTHON" /tmp/v/twine
 source "/tmp/v/twine/bin/activate"
 
 pip install twine
