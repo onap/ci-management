53681540fef42f0314a9b2d453a59dc86e1e767b
Hagop Bozawglanian <hagop.bozawglanian@att.com>
2020-01-20 15:29:53 -0800

    VNFRQTS - Updating jenkins jobs to use python3
    Need to update the jenkins jobs that build and upload artifacts
    to python 3 to run properly.
    
    Issue-ID: VNFRQTS-795
    
    Signed-off-by: Hagop Bozawglanian <hagop.bozawglanian@att.com>
    Change-Id: Ida5109c2b4bc395c6d65a2b5afbf19bdaa8bef8d

--- jjb/vnfrqts/pre-build-needs.sh
+++ jjb/vnfrqts/pre-build-needs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-virtualenv $WORKSPACE/venv-tox
+virtualenv -p python3.6 $WORKSPACE/venv-tox
 source $WORKSPACE/venv-tox/bin/activate
 pip install --upgrade pip
 pip install -r etc/requirements.txt
 ls
+wget -O docs/data/needs.json "https://nexus.onap.org/content/sites/raw/org.onap.vnfrqts.requirements/master/needs.json" && echo "Copied newest needs." || exit 1
 tox -e needs
