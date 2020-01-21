f15350c439fa1c972c271b05fd8352e4da0f3cb9
Hagop Bozawglanian <hagop.bozawglanian@att.com>
2020-01-21 11:06:06 -0800

    VVP - Updating jenkins jobs to use python3
    Need to update the jenkins jobs that build and upload artifacts
    to python 3 to run properly.
    
    Issue-ID: VVP-363
    
    Signed-off-by: Hagop Bozawglanian <hagop.bozawglanian@att.com>
    Change-Id: Ic0aae8065df2d4a4016d7fe8c51c4563f1bd253e

--- jjb/vvp/pre-build-csv.sh
+++ jjb/vvp/pre-build-csv.sh
@@ -5,6 +5,6 @@ source $WORKSPACE/venv-tox/bin/activate
 pip3 install --upgrade pip
 pip3 install --no-use-pep517 -r requirements.txt
 ls
-wget -O docs/data/needs.json "https://nexus.onap.org/content/sites/raw/org.onap.vnfrqts.requirements/master/needs.json" && echo "Copied newest needs." || exit 1
+wget -O $WORKSPACE/ice_validator/heat_requirements.json "https://nexus.onap.org/content/sites/raw/org.onap.vnfrqts.requirements/master/needs.json" && echo "Copied newest needs." || exit 1
 cd ice_validator/
 pytest --self-test tests/
