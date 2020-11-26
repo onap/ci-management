9a170053eb4b080f7741924d93770003b166e74e
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2020-11-26 10:25:39 +0100

    [OOM] add helm3 linting
    As OOM supports helm2 and helm3, it's important to check that all new
    additions don't break helm3 support.
    
    Issue-ID: OOM-1
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I38307e3b105606eb3dded1933e3a90673078d4dc
    Signed-off-by: Krzysztof Opasiak <k.opasiak@samsung.com>

--- shell/helm-repo-init.sh
+++ shell/helm-repo-init.sh
@@ -2,8 +2,7 @@
 # Ensure we fail the job if any steps fail
 set -e -o pipefail
 
-# client only init, tiller will not be installed
-helm init --client-only
-cd kubernetes/ || exit
-make repo
-cd ..
+mkdir -p ".chartstorage"
+chartmuseum --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &
+helm3 plugin install https://github.com/chartmuseum/helm-push.git
+helm3 repo add local http://localhost:6464
