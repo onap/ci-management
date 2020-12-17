768660df931ded9243fb67b25056d7c561d7eb3f
Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
2020-12-17 09:21:45 +0100

    Fix documentation generation in vnfsdk upload docs job
    Signed-off-by: Bartosz Gardziejewski <bartosz.gardziejewski@nokia.com>
    Change-Id: I4d8f26d771172404395af38cfee5e4784d3d3a68
    Issue-ID: VNFSDK-696

--- jjb/vnfsdk/upload-artifact.sh
+++ jjb/vnfsdk/upload-artifact.sh
@@ -5,7 +5,7 @@ then
     echo "ERROR: NO PROJECT ID ENTERED"
     exit 1
 else
-    cd $WORKSPACE/csarvalidation/target/generated-docs/Honolulu
+    cd $WORKSPACE/generated/active-validation-rules/Honolulu/
     for file in *.csv ; do
         echo "-n --upload-file $file https://nexus.onap.org/content/sites/raw/$PROJECT_ID/Honolulu/$file"
         curl -n --upload-file "$file" "https://nexus.onap.org/content/sites/raw/$PROJECT_ID/Honolulu/$file"
