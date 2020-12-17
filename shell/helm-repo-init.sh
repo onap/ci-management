81ed6e0ef1e6bdb0c10beb74f70d1ec176abad8c
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2020-12-17 13:45:39 +0100

    [OOM] use ramdisk to store chartmuseum packages
    it seems that chartmuseum may block if place where it stores the results
    is not fast enough.
    We then create a small ramdisk in order to store these.
    
    Issue-ID: OOM-1
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I36cad6e56f9390bb058beea08df0bf51eda97a09

--- shell/helm-repo-init.sh
+++ shell/helm-repo-init.sh
@@ -3,6 +3,7 @@
 set -e -o pipefail
 
 mkdir -p ".chartstorage"
-chartmuseum --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &
+sudo mount -t tmpfs -o size=64M tmpfs .chartstorage
+chartmuseum --debug --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &
 helm3 plugin install https://github.com/chartmuseum/helm-push.git
 helm3 repo add local http://localhost:6464
