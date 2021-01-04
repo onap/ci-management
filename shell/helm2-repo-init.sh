b92ab9811d473a6a5fc702909b8064f8ecaa21d7
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2021-01-04 11:03:25 +0100

    [OOM] Skip faulty install of helm2 stable repo
    Stable repo is not the right one and we also don't need it here for
    linting.
    So let's remove it at start.
    
    Issue-ID: OOM-1
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I2cdd105bf40c588bcea536fcb4af54ba54cdac14

--- shell/helm2-repo-init.sh
+++ shell/helm2-repo-init.sh
@@ -3,7 +3,8 @@
 set -e -o pipefail
 
 # client only init, tiller will not be installed
-helm init --client-only
+helm init --client-only --skip-refresh
+helm repo rm stable
 cd kubernetes/ || exit
 make repo
 cd ..
