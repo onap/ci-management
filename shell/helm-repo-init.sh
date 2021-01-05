9345ad89bb968787bef1a94cecb3d98256fde692
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2021-01-05 13:21:44 +0100

    [OOM] make helm3 linting to work
    chartmuseum was killed by Jenkins when launched. By moving it's outputs
    to /dev/null, it's no longer the case.
    
    Issue-ID: OOM-1
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: Ie358e73a498106ac3abf3731f47a1cb8f01deace

--- shell/helm-repo-init.sh
+++ shell/helm-repo-init.sh
@@ -3,7 +3,7 @@
 set -e -o pipefail
 
 mkdir -p ".chartstorage"
-sudo mount -t tmpfs -o size=128M tmpfs .chartstorage
-chartmuseum --debug --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &
+
+chartmuseum --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &> /dev/null &
 helm3 plugin install https://github.com/chartmuseum/helm-push.git
 helm3 repo add local http://localhost:6464
