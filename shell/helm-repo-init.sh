3c624dea37d410120c5eea0d49ba9abed914ce86
Sylvain Desbureaux <sylvain.desbureaux@orange.com>
2021-02-19 16:24:48 +0100

    [OOM] fix repo init script
    as helm3 binary is no more available, helm repo init script must use
    `$HELM_BIN` retrieved from prop file.
    
    Issue-ID: OOM-1
    Signed-off-by: Sylvain Desbureaux <sylvain.desbureaux@orange.com>
    Change-Id: I56de1ddf62488ef8bd0b4a177263adeeb5149947

--- shell/helm-repo-init.sh
+++ shell/helm-repo-init.sh
@@ -5,5 +5,6 @@ set -e -o pipefail
 mkdir -p ".chartstorage"
 
 chartmuseum --port=6464 --storage="local" --storage-local-rootdir=".chartstorage" &> /dev/null &
-helm3 plugin install https://github.com/chartmuseum/helm-push.git
-helm3 repo add local http://localhost:6464
+source helm.prop
+$HELM_BIN plugin install https://github.com/chartmuseum/helm-push.git
+$HELM_BIN repo add local http://localhost:6464
