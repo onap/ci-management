efc6d36f35cdae9e8a1847ef2a7e38be49a02ef2
dyh <dengyuanhong@chinamobile.com>
2021-06-17 11:55:54 +0800

    [VFC] remove JUJU driver
    Change-Id: Ife2a2cf95dd45aeb5b6e0fce6b9df31f3d5caf26
    Issue-ID: VFC-1868
    Signed-off-by: dyh <dengyuanhong@chinamobile.com>

--- jjb/vfc/build-gvnfm-driver-docker-image.sh
+++ jjb/vfc/build-gvnfm-driver-docker-image.sh
@@ -8,6 +8,7 @@ echo $CURRENTDIR
 chmod 755 ./gvnfmadapter/docker/*.*
 ./gvnfmadapter/docker/build_image.sh
 
-chmod 755 ./juju/juju-vnfmadapter/Juju-vnfmadapterService/docker/*.*
-./juju/juju-vnfmadapter/Juju-vnfmadapterService/docker/build_image.sh
+# juju-vnfmadapter is removed 
+# chmod 755 ./juju/juju-vnfmadapter/Juju-vnfmadapterService/docker/*.*
+# ./juju/juju-vnfmadapter/Juju-vnfmadapterService/docker/build_image.sh
 
