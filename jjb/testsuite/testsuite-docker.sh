b4f98de1934705d928e4ade445c3c1f0e2c01389
Michal Jagiello <michal.jagiello@t-mobile.pl>
2023-02-01 13:25:41 +0000

    [INTEGRAION][Testsuite] Add '--no-cache' flag to docker build command
    Do not use cache on docker build command as with it not always
    the latest changes were used to build an image
    
    Issue-ID: TEST-390
    Signed-off-by: Michal Jagiello <michal.jagiello@t-mobile.pl>
    Change-Id: I560d3f17860a2bf50caa743ee55d8f56d7fc1660

--- jjb/testsuite/testsuite-docker.sh
+++ jjb/testsuite/testsuite-docker.sh
@@ -2,7 +2,7 @@
 #
 echo '============== CALLING SCRIPT TO CREATE DOCKER IMAGES ================='
 cp $WORKSPACE/docker/* .
-docker -D build -t onap/testsuite .
+docker -D build --no-cache -t onap/testsuite .
 export REPO="nexus3.onap.org:10003"
 
 DATETIME_STAMP=$(date +%Y%m%dT%H%M%S)
