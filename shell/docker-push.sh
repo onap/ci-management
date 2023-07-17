128168dcb011258a41372f0c31aea3f6c9b0e5b3
Jessica Wagantall <jwagantall@linuxfoundation.org>
2023-07-17 12:48:40 -0700

    Fix: Remove local deprecated docker template
    The local docker build and merge jobs are deprecated and
    projects should use global-jjb templates instead.
    
    Having the old template was creating confusion in jjb 5.0.2
    where scripts were getting mixed. Is best to remove the old
    deprecated template.
    
    aai-common is migrated too to use the new template.
    
    Issue-ID: CIMAN-33
    Signed-off-by: Jessica Wagantall <jwagantall@linuxfoundation.org>
    Change-Id: I7d1cb277964636ea11cfceccf511ad2dc992dca3

--- shell/docker-push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Ensure we fail the job if any steps fail
-set -e -o pipefail
-
-# Push our image to wherever it's defined to go by the name
-docker push "$DOCKER_IMAGE"
-docker push "$DOCKER_IMAGE_LATEST"
